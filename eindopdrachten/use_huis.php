60ea85b919fe6051ad504cc25484eef31973a371
Lars van Vliet <123321@student.drenthecollege.nl>
2019-01-27 13:42:47 +0100

    Standaard object met alle eigenschappen

--- eindopdrachten/use_huis.php
+++ eindopdrachten/use_huis.php
@@ -4,8 +4,16 @@ spl_autoload_register(function ($class) {
     include 'classes/' . $class . '.class.php';
 });
 
-$huis1 = new Huis;
-echo $huis1->getEigenschappen('aantaldKamers', '5');
+$huis1 = new Huis('Appellaan', '5', 'Dorp');
+$huis1->setAantalKamers('10');
+$huis1->setAantalToiletten('2');
+$huis1->setVerwarming('True');
+$huis1->setSoortVerwarming('Vloerverwarming');
+$huis1->setVierkanteMeterGrond('500');
+$huis1->setWozWaarde('200.000');
+$huis1->setSoortDak('Puntdak');
+$huis1->setEnergielabel('A++');
+echo $huis1->getEigenschappen();
 
 
 ?>

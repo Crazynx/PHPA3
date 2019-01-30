dce1cd53d9e60f4660bdfa0cf5d63f5100e29577
Lars van Vliet <123321@student.drenthecollege.nl>
2019-01-30 15:09:04 +0100

    de vorige kaart wordt nu ook geplaatst in het spel

--- eindopdrachten/eindopdracht2/classes/kaart.class.php
+++ eindopdrachten/eindopdracht2/classes/kaart.class.php
@@ -2,25 +2,37 @@
 
 class Kaart extends Memory {
 
-  public function getKaarten($huidigeKaart) {
-    echo '<form action="" method="post">';
-    for ($x = 1; $x <= $this->aantalKaarten; $x++) {
-
-      if ($huidigeKaart == $this->kaartenNamen[$x-1]) {
-        echo '<input type="submit" value="O" name="kaart' . $x . '"></input>';
-      } else if ($vorigeKaart == $this->kaartenNamen[$x-1]) {
-        echo '<input type="submit" value="O" name="kaart' . $x . '"></input>';
-      } else {
+  public function getKaarten($huidigeKaart, $vorigeKaart, $clickCounter) {
+    if ($clickCounter == 3) {
+      echo '<form action="" method="post">';
+      for ($x = 1; $x <= $this->aantalKaarten; $x++) {
         echo '<input type="submit" value="" name="kaart' . $x . '"></input>';
-      }
 
-      if ($x == 4 || $x == 8 || $x == 12) {
-        echo '<br>';
+        if ($x == 4 || $x == 8 || $x == 12) {
+          echo '<br>';
+        }
       }
+      echo '</form>';
+
+    } else {
+      echo '<form action="" method="post">';
+      for ($x = 1; $x <= $this->aantalKaarten; $x++) {
+
+        if ($huidigeKaart == $this->kaartenNamen[$x-1]) {
+          echo '<input type="submit" value="O" name="kaart' . $x . '"></input>';
+        } else if ($vorigeKaart == $this->kaartenNamen[$x-1]) {
+          echo '<input type="submit" value="O" name="kaart' . $x . '"></input>';
+        } else {
+          echo '<input type="submit" value="" name="kaart' . $x . '"></input>';
+        }
+
+        if ($x == 4 || $x == 8 || $x == 12) {
+          echo '<br>';
+        }
+      }
+      echo '</form>';
     }
-    echo '</form>';
   }
-
 
 }
 
--- eindopdrachten/eindopdracht2/index.php
+++ eindopdrachten/eindopdracht2/index.php
@@ -1,20 +1,29 @@
 <?php
+session_start();
 spl_autoload_register(function ($class) {
     include 'classes/' . $class . '.class.php';
 });
 
 $memory = new Memory;
 $kaarten = new Kaart;
+$huidigeKaart = '';
 
 for ($x = 1; $x <= 16; $x++) {
 
   if (isset($_POST['kaart' . $x])) {
     $huidigeKaart = 'kaart' . $x;
+    $_SESSION['clickCounter'] += 1;
   }
 }
 
-echo $huidigeKaart;
-$kaarten->getKaarten($huidigeKaart);
+$kaarten->getKaarten($huidigeKaart, $_SESSION['vorigeKaart'], $_SESSION['clickCounter']);
+$_SESSION['vorigeKaart'] = $huidigeKaart; //Stelt de vorige kaart in, zodat deze na het submitten van de form blijft staan
+if ($_SESSION['clickCounter'] == 2) {
+  $_SESSION['clickCounter'] = 0; //Reset de clickCounter
+}
+
+echo $_SESSION['clickCounter'];
+
 
 
 ?>

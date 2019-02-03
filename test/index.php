6298eb00b58a74dd103c09443464ca6609926b71
Lars van Vliet <123321@student.drenthecollege.nl>
2019-02-03 13:01:15 +0100

    commentaar toegevoegd op de index pagina

--- test/index.php
+++ test/index.php
@@ -1,21 +1,20 @@
 <?php
 session_start();
-spl_autoload_register(function($class_name) {
+spl_autoload_register(function($class_name) { // Laad alle klassen in die we nodig hebben
   include 'classes/' . $class_name . '.class.php';
 });
 
 
-if (!isset($_SESSION['instance'])) {
-  $memory = new Memory;
-  $memory->setKaarten();
-  $_SESSION['kaartWaarden'] = serialize($memory->getKaartWaarden());
-  $_SESSION['kaartNamen'] = serialize($memory->getKaartNamen());
-  $_SESSION['instance'] = TRUE;
+if (!isset($_SESSION['instance'])) { // Kijkt of er al een instantie gemaakt is
+  $memory = new Memory; // Maak een nieuwe instantie van de klasse Memory
+  $memory->setKaarten(); // Stel de waarden in die we nodig hebben
+  $_SESSION['kaartWaarden'] = serialize($memory->getKaartWaarden()); // Stop de waarden in een array, zodat
+  $_SESSION['kaartNamen'] = serialize($memory->getKaartNamen()); // ze na een refresh gebruikt kunnen worden
+  $_SESSION['instance'] = TRUE; // Zet dit op true, zodat er niet nog een instantie gemaakt wordt
 }
 
-$kaartWaarden = unserialize($_SESSION['kaartWaarden']);
+$kaartWaarden = unserialize($_SESSION['kaartWaarden']); // Unserialize, zodat we het kunnen gebruiken in het spel
 $kaartNamen = unserialize($_SESSION['kaartNamen']);
 
-print_r($kaartWaarden);
 
 ?>

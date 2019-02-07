da567ca080ee0de09b8b53c82c72a9a4a4506b3e
Lars van Vliet <123321@student.drenthecollege.nl>
2019-02-07 13:51:02 +0100

    commentaar toegevoegd

--- finaltest/classes/kaart.class.php
+++ finaltest/classes/kaart.class.php
@@ -6,28 +6,39 @@ class Kaart {
   private $_kaartNamen;
   private $_gematchteKaarten;
 
-  public function __construct() {
+  public function __construct() { // Als er een object gemaakt wordt, dan worden deze methoden opgeroepen
     $this->_initialiseerKaartNamen();
     $this->_initialiseerKaartWaarden();
   }
 
   public function checkForMatch($huidigeKaart, $vorigeKaart) {
-    if ($this->_kaartWaarden[$huidigeKaart] == $this->_kaartWaarden[$vorigeKaart] && $huidigeKaart != $vorigeKaart) {
-      $_SESSION['gematchteKaarten'][$huidigeKaart] = $huidigeKaart;
-      $_SESSION['gematchteKaarten'][$vorigeKaart] = $vorigeKaart;
-      return true;
+      if ($this->_kaartWaarden[$huidigeKaart] == $this->_kaartWaarden[$vorigeKaart] && $huidigeKaart != $vorigeKaart) { // Een kaart kan niet met dezelfde kaart overeen komen
+        $_SESSION['gematchteKaarten'][$huidigeKaart] = $huidigeKaart; // Voeg de kaart toe aan de gematchteKaarten array
+        $_SESSION['gematchteKaarten'][$vorigeKaart] = $vorigeKaart; // Voeg de kaart toe aan de gematchteKaarten array
+        return true; // True, want er is een match gevonden
     }
   }
 
-  public function resetKaarten() {
+  public function toonAlleKaarten() { // Toon alle kaarten
+    echo '<form action="" method="post">';
+      for ($x = 0; $x < 16; $x++) {
+        echo '<input type="submit" name="'.$this->_kaartNamen[$x].'" value="'.$this->_kaartWaarden['kaart'.($x+1)].'"></input>';
+        if ($x==3 || $x==7|| $x==11) {
+          echo '<br>';
+        }
+      }
+    echo '</form>';
+  }
+
+  public function resetKaarten() { // Draai alle kaarten om
     echo '<form action="" method="post">';
       for ($x = 1; $x <= 16; $x++) {
-        if (isset($_SESSION['gematchteKaarten']['kaart'.$x])) {
+        if (isset($_SESSION['gematchteKaarten']['kaart'.$x])) { // Kijkt of de waarde in de gematchteKaarten array staat, zodat die kaart "omgedraaid" is
           echo '<input type="submit" name="'.$this->_kaartNamen[$x-1].'" value="'.$this->_kaartWaarden[$this->_kaartNamen[$x-1]].'"></input>';
-        } else {
+        } else { // Anders wordt er een "kale" kaart getoont
           echo '<input type="submit" name="'.$this->_kaartNamen[$x-1].'" value=""></input>';
         }
-        if ($x==4||$x==8||$x==12) {
+        if ($x==4||$x==8||$x==12) { // Zorgt voor de linebreaks
           echo '<br>';
         }
       }
@@ -35,14 +46,20 @@ class Kaart {
   }
 
   public function updateKaarten($huidigeKaart, $vorigeKaart, $klikCounter) {
-    if ($klikCounter == 3) {
+    if ($klikCounter == 2) { // Als er 3 keer geklikt is
       $this->resetKaarten();
       $_SESSION['klikCounter'] = 0; // Reset de klik counter
     } else {
       echo '<form action="" method="post">';
         for ($x = 0; $x < 16; $x++) {
-          echo '<input type="submit" name="'.$this->_kaartNamen[$x].'" value="'.$this->_kaartWaarden[$this->_kaartNamen[$x]].'"></input>';
-          if ($x==3||$x==7||$x==11) {
+          if ($huidigeKaart == 'kaart'.($x+1) || $vorigeKaart == 'kaart'.($x+1)) {
+            echo '<input type="submit" name="'.$this->_kaartNamen[$x].'" value="'.$this->_kaartWaarden['kaart'.($x+1)].'"></input>';
+          } else if (isset($_SESSION['gematchteKaarten']['kaart'.($x+1)])) { // Kijkt of de waarde in de gematchteKaarten array staat, zodat die kaart "omgedraaid" is
+            echo '<input type="submit" name="'.$this->_kaartNamen[$x].'" value="'.$this->_kaartWaarden['kaart'.($x+1)].'"></input>';
+          } else { // Anders wordt er een "kale" kaart getoont
+            echo '<input type="submit" name="'.$this->_kaartNamen[$x].'" value=""></input>';
+          }
+          if ($x==3||$x==7||$x==11) { // Zorgt voor de linebreaks
             echo '<br>';
           }
         }
@@ -50,20 +67,18 @@ class Kaart {
     }
   }
 
-  private function _initialiseerKaartNamen() {
+  private function _initialiseerKaartNamen() { // Stelt de naam array in
     for ($x = 0; $x < 16; $x++) {
       $this->_kaartNamen[$x] = 'kaart'.($x+1);
     }
   }
 
-  private function _initialiseerKaartWaarden() {
+  private function _initialiseerKaartWaarden() { // Stelt de waarden in die bij elke kaart hoort
     for ($x = 0; $x < 16; $x++) {
       $randNum = rand(1,100);
       $this->_kaartWaarden[$this->_kaartNamen[$x]] = $randNum;
-      $this->_kaartWaarden[$this->_kaartNamen[++$x]] = $randNum;
+      $this->_kaartWaarden[$this->_kaartNamen[15-$x]] = $randNum;
     }
   }
-
 }
-
 ?>
--- finaltest/index.php
+++ finaltest/index.php
@@ -1,28 +1,39 @@
 <?php
-session_start();
-spl_autoload_register(function($class_name) {
+session_start(); // Begin de sessie
+spl_autoload_register(function($class_name) { // Laad alle nodige klassen
   require 'classes/'.$class_name.'.class.php';
 });
 
-// unset($_SESSION['instance']);
-// session_destroy();
+if (isset($_POST['resetSession'])) { // Herstart het spel
+  session_destroy();
+}
 
-if (!isset($_SESSION['instance'])) { // Eenmalig een object maken en andere dingen die maar één keer moeten gebeuren
+if (!isset($_SESSION['instance'])) { // Eenmalig een object maken en andere dingen doen die maar één keer moeten gebeuren
   $kaart = new Kaart;
+  $kaart->toonAlleKaarten();
   $_SESSION['kaart'] = serialize($kaart);
   $_SESSION['instance'] = true;
+} else { // Als er al een object gemaakt is dan gebeurt dit
+  $form = new Form;
+  $huidigeKaart = $form->getPressedCard();
+  $_SESSION['klikCounter']++;
+
+  $kaart = unserialize($_SESSION['kaart']); // Het object terugzetten naar een variabele
+  $kaart->checkForMatch($huidigeKaart, $_SESSION['vorigeKaart']);
+  $kaart->updateKaarten($huidigeKaart, $_SESSION['vorigeKaart'], $_SESSION['klikCounter']);
+  $_SESSION['vorigeKaart'] = $huidigeKaart; // Stel de vorige geselecteerde kaart in
 }
-
-$form = new Form;
-$huidigeKaart = $form->getPressedCard();
-$_SESSION['klikCounter']++;
-
-
-$kaart = unserialize($_SESSION['kaart']); // Het object terugzetten naar een variabele
-if ($kaart->checkForMatch($huidigeKaart, $_SESSION['vorigeKaart'])) {
-  echo 'Er is een match!';
-}
-$kaart->updateKaarten($huidigeKaart, $_SESSION['vorigeKaart'], $_SESSION['klikCounter']);
-$_SESSION['vorigeKaart'] = $huidigeKaart; // Stel de vorige geselecteerde kaart in
-
 ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+<meta charset="utf-8">
+<title> Memory game </title>
+<link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+  <form class="destroySession" action="" method="post">
+    <input type="submit" name="resetSession" value="Opnieuw"></input>
+  </form>
+</body>
+</html>
